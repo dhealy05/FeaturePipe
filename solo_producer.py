9f3603e32b8218e74385dbdff9bdacca1ba56be6
Daniel Healy <danielhealy@Daniels-MacBook-Pro.local>
2020-02-03 07:18:03 -0800

    change to coroutine

--- solo_producer.py
+++ solo_producer.py
@@ -17,9 +17,7 @@ class Stock(Record):
     time = Long()
     #conditions = List()
 
-queue = asyncio.Queue(loop=loop)
-
-async def produce(ws):
+async def produce(queue, ws):
     while True:
         result = ws.recv()
         await queue.put(result)
@@ -28,11 +26,11 @@ async def produce(ws):
         #stock = Stock(symbol = final['sym'], exchange_id = final['x'], trade_id = final['i'], price = final['p'], tape = final['z'], size = final['s'], time = final['t'])
         #producer.send(stock)
 
-async def consume(producer):
+async def consume(queue, producer):
     while True:
         # wait for an item from the producer
         result = await queue.get()
-
+        print(result)
         json_result = json.loads(result)
         final = json_result[0]
         stock = Stock(symbol = final['sym'], exchange_id = final['x'], trade_id = final['i'], price = final['p'], tape = final['z'], size = final['s'], time = final['t'])
@@ -53,14 +51,20 @@ auth_json = json.dumps(auth)
 ws.send(auth_json)
 response = ws.recv()
 
-subscribe = {"action":"subscribe","params":"T."+symbol}
+subscribe = {"action":"subscribe","params":"T.MSFT"}
 subscribe_json = json.dumps(subscribe)
 ws.send(subscribe_json)
 response = ws.recv()
 
 loop = asyncio.get_event_loop()
+queue = asyncio.Queue(loop=loop)
 
-loop.create_task(produce(ws))
-loop.create_task(consume(producer))
+loop.create_task(produce(queue, ws))
+loop.create_task(consume(queue, producer))
 
-loop.run_forever()
+producer_coroutine = produce(queue, ws)
+consumer_coroutine = consume(queue, producer)
+loop.run_forever(asyncio.gather(producer_coroutine, consumer_coroutine))
+#loop.close()
+
+#loop.run_forever()
