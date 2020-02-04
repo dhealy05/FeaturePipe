a464fc0137ac7f6b70de44a03c1a00f7422f08b8
Daniel Healy <danielhealy@Daniels-MacBook-Pro.local>
2020-02-04 08:41:05 -0800

    pickle individual producers

--- produce_raw_data.py
+++ produce_raw_data.py
@@ -11,7 +11,8 @@ import pickle
 import os.path
 
 API_KEY = "6deAryjhAoa53eNJ5hMZSQb8BOKp64kpuHmYfa"
-PRODUCER_PATH = "./data/producers.pkl"
+PRODUCER_DICT_PATH = "./data/producer_dictionary.pkl"
+PRODUCER_PATH = "./data/producers/"
 
 client = pulsar.Client('pulsar://10.0.0.7:6650,10.0.0.8:6650,10.0.0.9:6650')
 
@@ -31,8 +32,8 @@ class Stock(Record):
     #conditions = List()
 
 def get_producer_dictionary():
-    if os.path.isfile(PRODUCER_PATH):
-        with open(PRODUCER_PATH, 'rb') as input:
+    if os.path.isfile(PRODUCER_DICT_PATH):
+        with open(PRODUCER_DICT_PATH, 'rb') as input:
             producer_dictionary = pickle.load(input)
     else:
         init_producers()
@@ -42,13 +43,23 @@ def init_producers():
     tickers = get_tickers()
 
     for ticker in tickers:
-        create_producer(ticker)
+        if os.path.isfile(PRODUCER_PATH + ticker + ".pkl"):
+            with open(PRODUCER_PATH + ticker + ".pkl", 'rb') as input:
+                producer_dictionary[ticker] = pickle.load(input)
+        else:
+            create_producer(ticker)
 
     with open(PRODUCER_PATH, 'wb') as output:
         pickle.dump(producer_dictionary, output, pickle.HIGHEST_PROTOCOL)
 
 def create_producer(ticker):
-    producer_dictionary[ticker] = client.create_producer(ticker, schema=AvroSchema(Stock))
+
+    producer = client.create_producer(ticker, schema=AvroSchema(Stock))
+
+    producer_dictionary[ticker] = producer
+
+    with open(PRODUCER_PATH + ticker + ".pkl", 'wb') as output:
+        pickle.dump(producer, output, pickle.HIGHEST_PROTOCOL)
 
 def init_websocket():
 
