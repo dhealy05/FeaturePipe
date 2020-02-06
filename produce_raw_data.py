b82a2a9834a005b84246d63de296a52a99cf8e1c
Daniel Healy <danielhealy@Daniels-MacBook-Pro.local>
2020-02-06 12:19:02 -0800

    testing broker persistence error--try just 1 producer

--- produce_raw_data.py
+++ produce_raw_data.py
@@ -38,7 +38,7 @@ class Stock(Record):
 def init_producers():
 
     producer_dictionary["all_stocks"] = client.create_producer("all_stocks", schema=AvroSchema(Stock))
-
+    return
     count = 0
 
     #tickers = get_tickers()
@@ -100,7 +100,7 @@ def send_message(result):
                     stock = make_stock(final)
 
                     if ticker in final_tickers:
-                        producer_dictionary[ticker].send(stock)
+                        #producer_dictionary[ticker].send(stock)
                         producer_dictionary["all_stocks"].send(stock)
 
 def on_message(ws, message):
