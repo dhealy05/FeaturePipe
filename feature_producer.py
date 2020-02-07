6606aee338a1eec05bf858e641d473a317e49d75
Daniel Healy <danielhealy@Daniels-MacBook-Pro.local>
2020-02-07 11:16:12 -0800

    schedule features

--- feature_producer.py
+++ feature_producer.py
@@ -30,25 +30,7 @@ producer_count = 0
 #feature_set = ['ma_1', 'ma_5', 'std_1', 'std_5', 'vol_1', 'vol_5']
 feature_set = ['avg_1', 'avg_5', 'stddev_1', 'stddev_5']
 
-cursor = presto.connect('10.0.0.10', port=8081, username="djh").cursor()
-
 from threading import Thread, Lock
-
-def get_feature(action, num_minutes):
-    #action == AVG or action == STDDEV
-    seconds = time.time()
-    boundary = (seconds - (60*num_minutes))
-    query = 'SELECT ' + action + '(price), symbol, price FROM pulsar."public/default".all_stocks WHERE time > ' + str(boundary) + ' GROUP BY symbol'
-    cursor.execute(query)
-    result = cursor.fetchall()
-    return result
-    #print cursor.fetchall()
-    #send_message(result[0])
-
-def get_all_features():
-    for feature in feature_set:
-        action, num_minutes = feature.split("_")
-        global_features[feature] = get_feature(action, num_minutes)
 
 def get_all_queries():
 
@@ -57,8 +39,9 @@ def get_all_queries():
 
     for feature in feature_set:
         action, num_minutes = feature.split("_")
+        num_minutes = int(num_minutes)
         boundary = (seconds - (60*num_minutes))
-        query = 'SELECT ' + action + '(price), symbol, price FROM pulsar."public/default".all_stocks WHERE time > ' + str(boundary) + ' GROUP BY symbol'
+        query = 'SELECT ' + action + '(price), symbol FROM pulsar."public/default".all_stocks WHERE time > ' + str(boundary) + ' GROUP BY symbol'
         queries.append(query)
 
     return queries
@@ -78,41 +61,45 @@ class QueryWorker(Thread):
     def run(self):
 
         result = None
-        logging.info("Connecting to database...")
+        print("Connecting to database...")
 
         try:
+            cursor = presto.connect('10.0.0.10', port=8081, username="djh").cursor()
             cursor.execute(query)
             result = cursor.fetchall()
         except Exception as e:
-            logging.error("Unable to access database %s" % str(e))
+            print("Unable to access database %s" % str(e))
 
         self.result_queue.append(result)
 
-delay = 1
-result_queue = []
+def run_all_queries():
 
-workers = []
-queries = get_all_queries()
+    delay = 1
+    result_queue = []
 
-for query in queries:
-    worker = QueryWorker(query, result_queue)
-    workers.append(worker)
+    workers = []
+    queries = get_all_queries()
 
-for worker in workers:
-    worker.start()
+    for query in queries:
+        worker = QueryWorker(query, result_queue)
+        workers.append(worker)
 
-# Wait for the job to be done
-while len(result_queue) < len(feature_set):
-    sleep(delay)
+    for worker in workers:
+        worker.start()
 
-job_done = True
+    # Wait for the job to be done
+    while len(result_queue) < len(feature_set):
+        sleep(delay)
 
-for worker in workers:
-    worker.join()
+    job_done = True
 
-print(result_queue)    
+    for worker in workers:
+        worker.join()
 
-#schedule.every(10).seconds.do(get_all_averages)
+    for results in result_queue:
+        print(results[0])
 
-#while True:
-#    schedule.run_pending()
+schedule.every(15).seconds.do(run_all_queries)
+
+while True:
+    schedule.run_pending()
